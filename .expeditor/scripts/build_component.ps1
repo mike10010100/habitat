95f5a49d23d11f220f296451e58c38dfaaae07aa
Scott Hain <shain@chef.io>
2019-05-09 12:49:27 -0700

    moar

--- .expeditor/scripts/build_component.ps1
+++ .expeditor/scripts/build_component.ps1
@@ -13,12 +13,24 @@ if($Component.Equals("")) {
   Write-Error "--- :error: Component to build not specified, please use the -Component flag"
 }
 
-& choco install habitat -y
+$destination_channel = $Env:BUILDKITE_BUILD_ID
 
-& hab pkg install core/hab
+$Env:HAB_LICENSE = "accept-no-persist"
+$Env:HAB_STUDIO_SECRET_HAB_LICENSE = "accept-no-persist"
+
+choco install habitat -y
+
+hab pkg install core/hab
 
 $hab_bin_path = & hab pkg path core/hab
 $hab_binary="$hab_bin_path/bin/hab"
 $hab_binary_version = & $hab_binary --version
 
 Write-Host "--- Using habitat version $hab_binary_version"
+
+Write-Host "--- Running a build $HAB_ORIGIN / $Component / $destination_channel"
+$hab_binary origin key download $HAB_ORIGIN
+$hab_binary origin key download --auth $SCOTTHAIN_HAB_AUTH_TOKEN --secret $HAB_ORIGIN
+
+
+exit $LASTEXITCODE
