fbdcf518719ca27d4af04b62391b511f1bb0c9d0
Scott Hain <shain@chef.io>
2019-07-03 11:00:04 -0700

    thingerys

--- .expeditor/scripts/setup_environment.sh
+++ .expeditor/scripts/setup_environment.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 # source .buildkite/scripts/shared.sh
 
+
+# `channel` should be channel we are pulling from
+#
+# e.g. `DEV`, `ACCEPTANCE` etc.
+channel=${1}
+
 # This currently overrides some functions from the pure buildkite
 # shared.sh file above. As we migrate, more things will be added to
 # this file.
@@ -11,15 +17,14 @@ set -euo pipefail
 
 export HAB_AUTH_TOKEN="${ACCEPTANCE_HAB_AUTH_TOKEN}"
 export HAB_BLDR_URL="${ACCEPTANCE_HAB_BLDR_URL}"
+export CI_OVERRIDE_CHANNEL="${channel}"
+export HAB_BLDR_CHANNEL="${channel}"
 
 # This is kinda silly
+echo "--- Installing base hab using curl|bash"
 curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
 
-# `channel` should be channel we are pulling from
-#
-# e.g. `DEV`, `ACCEPTANCE` etc.
-channel=${1}
-
+echo "--- Installing latest core/hab from ${channel}"
 hab pkg install --binlink --force --channel ${channel} core/hab
 
 hab --version
